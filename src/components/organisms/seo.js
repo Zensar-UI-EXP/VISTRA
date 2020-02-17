9e0723402734cfb76d0f3c11454626541880ac44
kmkmathan <kmkmathan@gmail.com>
2020-02-17 19:10:39 +0530

    spot light removed

--- src/components/organisms/seo.js
+++ src/components/organisms/seo.js
@@ -9,7 +9,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
-import "../../assets/spotlight-slider.js";
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
